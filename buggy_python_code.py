c31ed2b8d7df28d7eefd88842c5915e08e796b15
zitnanm <zitnanm4@gmail.com>
2025-04-30 08:38:13 +0200

    fixed some issues buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,7 @@
-import sys 
-import os
+import sys
 import yaml
 import flask
+import urllib3
 
 app = flask.Flask(__name__)
 
@@ -12,9 +12,8 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     def __init__(self, name):
         self.name = name
 
@@ -28,7 +27,7 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try: 
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -45,13 +44,14 @@ def authenticate(password):
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
+
 if __name__ == '__main__':
     print("Vulnerabilities:")
     print("1. Format string vulnerability:")
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice  = input("Select vulnerability: ")
+    choice = input("Select vulnerability: ")
     if choice == "1": 
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
@@ -64,4 +64,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
